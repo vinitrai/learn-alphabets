48e5219a41240f59db933f463c94442bbff9d26e
vinit <learner.vinit@gmail.com>
2021-10-03 07:32:51 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,733 @@
+// our data for this project -an array of objects
+const alphabets=[
+{
+
+    title:'A',
+    details:{
+        animal:{
+            name:'alligator',
+            src:'./images/animals/alligator.jpg'
+        },
+        bird:{
+            name:'anhinga',
+            src:'./images/birds/anhinga.jpg'
+        },
+        flower:{
+            name:'aconitum',
+            src:'./images/flowers/aconitum.jpg'
+        },
+        fruit:{
+            name:'apple',
+            src:'./images/fruits/apple.jpg'
+        }
+    }
+},
+{
+
+
+    title:'B',
+    details:{
+        animal:{
+            name:'bear',
+            src:'./images/animals/bear.jpg'
+        },
+        bird:{
+            name:'blue bird',
+            src:'./images/birds/bluebird.jpg'
+        },
+        flower:{
+            name:'baby breathe',
+            src:'./images/flowers/babybreathe.jpg'
+        },
+        fruit:{
+            name:'banana',
+            src:'./images/fruits/banana.jpg'
+        }
+    }
+},
+{
+
+
+    title:'C',
+    details:{
+        animal:{
+            name:'cat',
+            src:'./images/animals/cat.jpg'
+        },
+        bird:{
+            name:'chickadee',
+            src:'./images/birds/chickadee.jpg'
+        },
+        flower:{
+            name:'candy tuft',
+            src:'./images/flowers/candytuft.jpg'
+        },
+        fruit:{
+            name:'cherry',
+            src:'./images/fruits/cherry.jpg'
+        }
+    }
+},
+{
+
+
+    title:'D',
+    details:{
+        animal:{
+            name:'dog',
+            src:'./images/animals/dog.jpg'
+        },
+        bird:{
+            name:'duck',
+            src:'./images/birds/duck.jpg'
+        },
+        flower:{
+            name:'daffodil',
+            src:'./images/flowers/daffodil.jpg'
+        },
+        fruit:{
+            name:'date',
+            src:'./images/fruits/dates.jpg'
+        }
+    }
+},
+{
+
+
+    title:'E',
+    details:{
+        animal:{
+            name:'elephant',
+            src:'./images/animals/elephant.jpg'
+        },
+        bird:{
+            name:'egret',
+            src:'./images/birds/egret.jpg'
+        },
+        flower:{
+            name:'erica',
+            src:'./images/flowers/erica.jpg'
+        },
+        fruit:{
+            name:'eggplant',
+            src:'./images/fruits/eggplant.jpg'
+        }
+    }
+},
+{
+
+
+
+    title:'F',
+    details:{
+        animal:{
+            name:'fox',
+            src:'./images/animals/fox.jpg'
+        },
+        bird:{
+            name:'falcon',
+            src:'./images/birds/falcon.jpg'
+        },
+        flower:{
+            name:'flax flower',
+            src:'./images/flowers/flaxflower.jpg'
+        },
+        fruit:{
+            name:'fig',
+            src:'./images/fruits/figs.jpg'
+        }
+    }
+},
+{
+
+
+
+    title:'G',
+    details:{
+        animal:{
+            name:'goat',
+            src:'./images/animals/goat.jpg'
+        },
+        bird:{
+            name:'gallinule',
+            src:'./images/birds/gallinule.jpg'
+        },
+        flower:{
+            name:'gerbera',
+            src:'./images/flowers/gerbera.jpg'
+        },
+        fruit:{
+            name:'grape',
+            src:'./images/fruits/grape.jpg'
+        }
+    }
+},
+{
+
+
+
+    title:'H',
+    details:{
+        animal:{
+            name:'horse',
+            src:'./images/animals/horse.jpg'
+        },
+        bird:{
+            name:'humming bird',
+            src:'./images/birds/hummingbird.jpg'
+        },
+        flower:{
+            name:'heather',
+            src:'./images/flowers/heather.jpg'
+        },
+        fruit:{
+            name:'honeydew',
+            src:'./images/fruits/honeydew.jpg'
+        }
+    }
+},
+{
+
+
+
+    title:'I',
+    details:{
+        animal:{
+            name:'indri',
+            src:'./images/animals/indri.jpg'
+        },
+        bird:{
+            name:'ibis',
+            src:'./images/birds/ibis.jpg'
+        },
+        flower:{
+            name:'ice plant',
+            src:'./images/flowers/iceplant.jpg'
+        },
+        fruit:{
+            name:'ita palm',
+            src:'./images/fruits/itapalm.jpg'
+        }
+    }
+},
+{
+
+
+
+    title:'J',
+    details:{
+        animal:{
+            name:'jaquar',
+            src:'./images/animals/jaquar.jpg'
+        },
+        bird:{
+            name:'jay',
+            src:'./images/birds/jay.jpg'
+        },
+        flower:{
+            name:'jasmine',
+            src:'./images/flowers/jasmine.jpg'
+        },
+        fruit:{
+            name:'jujube',
+            src:'./images/fruits/jujube.jpg'
+        }
+    }
+},
+{
+
+
+
+    title:'K',
+    details:{
+        animal:{
+            name:'kangaroo',
+            src:'./images/animals/kangaroo.jpg'
+        },
+        bird:{
+            name:'kingfisher',
+            src:'./images/birds/kingfisher.jpg'
+        },
+        flower:{
+            name:'kamine',
+            src:'./images/flowers/kamine.jpg'
+        },
+        fruit:{
+            name:'kiwi',
+            src:'./images/fruits/kiwis.jpg'
+        }
+    }
+},
+{
+
+
+
+    title:'L',
+    details:{
+        animal:{
+            name:'lion',
+            src:'./images/animals/lion.jpg'
+        },
+        bird:{
+            name:'loon',
+            src:'./images/birds/loon.jpg'
+        },
+        flower:{
+            name:'lavender',
+            src:'./images/flowers/lavender.jpg'
+        },
+        fruit:{
+            name:'lemon',
+            src:'./images/fruits/lemon.jpg'
+        }
+    }
+},
+{
+
+
+
+    title:'M',
+    details:{
+        animal:{
+            name:'monkey',
+            src:'./images/animals/monkey.jpg'
+        },
+        bird:{
+            name:'meadow lark',
+            src:'./images/birds/meadowlark.jpg'
+        },
+        flower:{
+            name:'mallow',
+            src:'./images/flowers/mallow.jpg'
+        },
+        fruit:{
+            name:'mango',
+            src:'./images/fruits/mango.jpg'
+        }
+    }
+},
+{
+
+
+
+    title:'N',
+    details:{
+        animal:{
+            name:'narwhal',
+            src:'./images/animals/narwhal.jpg'
+        },
+        bird:{
+            name:'nighthawk',
+            src:'./images/birds/nighthawk.jpg'
+        },
+        flower:{
+            name:'nemesia',
+            src:'./images/flowers/nemesia.jpg'
+        },
+        fruit:{
+            name:'nectarine',
+            src:'./images/fruits/nectarines.jpg'
+        }
+    }
+},
+{
+
+
+
+    title:'O',
+    details:{
+        animal:{
+            name:'octopus',
+            src:'./images/animals/octopus.jpg'
+        },
+        bird:{
+            name:'owl',
+            src:'./images/birds/owl.jpg'
+        },
+        flower:{
+            name:'orchid',
+            src:'./images/flowers/orchid.jpg'
+        },
+        fruit:{
+            name:'orange',
+            src:'./images/fruits/orange.jpg'
+        }
+    }
+},
+{
+
+
+
+    title:'P',
+    details:{
+        animal:{
+            name:'pig',
+            src:'./images/animals/pig.jpg'
+        },
+        bird:{
+            name:'pelican',
+            src:'./images/birds/pelican.jpg'
+        },
+        flower:{
+            name:'pansy',
+            src:'./images/flowers/pansy.jpg'
+        },
+        fruit:{
+            name:'papaya',
+            src:'./images/fruits/papaya.jpg'
+        }
+    }
+},
+{
+
+
+
+    title:'Q',
+    details:{
+        animal:{
+            name:'quokka',
+            src:'./images/animals/quokka.jpg'
+        },
+        bird:{
+            name:'qual',
+            src:'./images/birds/qual.jpg'
+        },
+        flower:{
+            name:'quince',
+            src:'./images/flowers/quince.jpg'
+        },
+        fruit:{
+            name:'quince',
+            src:'./images/fruits/quince.jpg'
+        }
+    }
+},
+{
+
+
+
+    title:'R',
+    details:{
+        animal:{
+            name:'rabbit',
+            src:'./images/animals/rabbit.jpg'
+        },
+        bird:{
+            name:'raven',
+            src:'./images/birds/raven.jpg'
+        },
+        flower:{
+            name:'rose',
+            src:'./images/flowers/rose.jpg'
+        },
+        fruit:{
+            name:'raspberry',
+            src:'./images/fruits/raspberry.jpg'
+        }
+    }
+},
+{
+
+
+
+    title:'S',
+    details:{
+        animal:{
+            name:'sheep',
+            src:'./images/animals/sheep.jpg'
+        },
+        bird:{
+            name:'skimmer',
+            src:'./images/birds/skimmer.jpg'
+        },
+        flower:{
+            name:'sunflower',
+            src:'./images/flowers/sunflower.jpg'
+        },
+        fruit:{
+            name:'strawberry',
+            src:'./images/fruits/strawberry.jpg'
+        }
+    }
+},
+{
+
+
+
+    title:'T',
+    details:{
+        animal:{
+            name:'tiger',
+            src:'./images/animals/tiger.jpg'
+        },
+        bird:{
+            name:'tanager',
+            src:'./images/birds/tanager.jpg'
+        },
+        flower:{
+            name:'tiger lily',
+            src:'./images/flowers/tigerlily.jpg'
+        },
+        fruit:{
+            name:'tomato',
+            src:'./images/fruits/tomato.jpg'
+        }
+    }
+},
+{
+
+
+
+    title:'U',
+    details:{
+        animal:{
+            name:'uakari',
+            src:'./images/animals/uakari.jpg'
+        },
+        bird:{
+            name:'upland sandpiper',
+            src:'./images/birds/upland-sandpiper.jpg'
+        },
+        flower:{
+            name:'ursinia',
+            src:'./images/flowers/ursinia.jpg'
+        },
+        fruit:{
+            name:'ugli fruit',
+            src:'./images/fruits/uglifruit.jpg'
+        }
+    }
+},
+{
+
+
+
+    title:'V',
+    details:{
+        animal:{
+            name:'vaquita',
+            src:'./images/animals/vaquita.jpg'
+        },
+        bird:{
+            name:'vulture',
+            src:'./images/birds/vulture.jpg'
+        },
+        flower:{
+            name:'vinca minor',
+            src:'./images/flowers/vincaminor.jpg'
+        },
+        fruit:{
+            name:'vovanga',
+            src:'./images/fruits/vovanga.jpg'
+        }
+    }
+},
+{
+
+
+
+    title:'W',
+    details:{
+        animal:{
+            name:'wolf',
+            src:'./images/animals/wolf.jpg'
+        },
+        bird:{
+            name:'woodpicker',
+            src:'./images/birds/woodpicker.jpg'
+        },
+        flower:{
+            name:'wall flower',
+            src:'./images/flowers/wallflower.jpg'
+        },
+        fruit:{
+            name:'water melon',
+            src:'./images/fruits/watermelon.jpg'
+        }
+    }
+},
+{
+
+
+
+    title:'X',
+    details:{
+        animal:{
+            name:'xerus',
+            src:'./images/animals/xerus.jpg'
+        },
+        bird:{
+            name:'xantus',
+            src:'./images/birds/xantus.jpg'
+        },
+        flower:{
+            name:'xylobium',
+            src:'./images/flowers/xylobium.jpg'
+        },
+        fruit:{
+            name:'xigua melon',
+            src:'./images/fruits/xiguamelon.jpg'
+        }
+    }
+},
+{
+
+
+
+    title:'Y',
+    details:{
+        animal:{
+            name:'yak',
+            src:'./images/animals/yak.jpg'
+        },
+        bird:{
+            name:'yellow leg',
+            src:'./images/birds/yellowleg.jpg'
+        },
+        flower:{
+            name:'yarrow',
+            src:'./images/flowers/yarrow.jpg'
+        },
+        fruit:{
+            name:'zucchini',
+            src:'./images/fruits/zucchini.jpg'
+        }
+    }
+},
+{
+
+
+    title:'Z',
+    details:{
+        animal:{
+            name:'zebra',
+            src:'./images/animals/zebra.jpg'
+        },
+        bird:{
+            name:'zone',
+            src:'./images/birds/zone.jpg'
+        },
+        flower:{
+            name:'zephyranthes',
+            src:'./images/flowers/zephyranthes.jpg'
+        },
+        fruit:{
+            name:'cherry',
+            src:'./images/fruits/cherry.jpg'
+        }
+    }
+}
+];
+
+
+
+
+//function to check for vowel
+function isVowel(str){
+    const vowels = ['a','e','i','o','u','A','E','I','O','U'];
+    let text='';
+    if(vowels.includes(str[0])){
+       text = `an ${str} pic`;
+   } else{
+       text = `a ${str} pic`
+   }
+   return text;
+    
+}
+// function to get the speech/voice
+function speakNow(textToSpeak){
+        const speech = new SpeechSynthesisUtterance();
+        speech.text = textToSpeak;
+        window.speechSynthesis.speak(speech);
+}
+//function to add transition and play sound
+function addEffects(){
+         const ourText = this.parentElement.previousElementSibling.textContent+' for '+ this.children[0].textContent;
+         this.classList.add('current');
+         speakNow(ourText);
+    }
+//function to stop the transition
+function removeTransition(e){
+        if(e.propertyName === 'transform'){
+            this.classList.remove('current');
+        }
+    
+}
+//function to play with keyword
+function playNow(e){
+const containers = document.querySelectorAll('.letter__container');
+[...containers].forEach(container=>{
+    const title = container.children[0];
+    const contents = container.children[1];
+    if(e.keyCode === title.textContent.charCodeAt()){
+        container.scrollIntoView();
+       speakNow(title.textContent);
+    }
+    
+})
+}
+
+// get the container that will hold our generated HTML
+const container = document.getElementById('container');
+
+// code to generate the HTML
+const ourData = alphabets.map(item=>{
+    //get the names 
+    const animal = item.details.animal.name;
+    const bird = item.details.bird.name;
+    const flower = item.details.flower.name;
+    const fruit = item.details.fruit.name;
+    //check if name starts with a vowel
+    let animalAlt = isVowel(animal);
+    let birdAlt = isVowel(bird);
+    let flowerAlt = isVowel(flower);
+    let fruitAlt = isVowel(fruit);
+    
+return `<div class="letter__container">
+            <h2 class="main__heading">${item.title}</h2>
+            <div class="content__container">
+            <div class="content animal">
+                <h3 class="content__title">${animal}</h3>
+                <div class="image__container">
+                    <img src="${item.details.animal.src}" alt="${animalAlt}">
+                </div>
+            </div>
+            <div class="content bird">
+                <h3 class="content__title">${bird}</h3>
+                <div class="image__container">
+                    <img src="${item.details.bird.src}" alt="${birdAlt}">
+                </div>
+            </div>
+            <div class="content flower">
+                <h3 class="content__title">${flower}</h3>
+                <div class="image__container">
+                    <img src="${item.details.flower.src}" alt="${flowerAlt}">
+                </div>
+        
+            </div>
+            <div class="content fruits">
+                <h3 class="content__title">${fruit}</h3>
+                <div class="image__container">
+                    <img src="${item.details.fruit.src}" alt="${fruitAlt}">
+                </div>
+            </div>
+            </div>
+        </div>`
+})
+
+
+// render the html on the page
+container.innerHTML += ourData.join('');
+
+
+
+
+//listening for the events
+const contents = document.querySelectorAll('.content');
+[...contents].forEach(content=>{
+    content.addEventListener('click',addEffects)
+     content.addEventListener('transitionend',removeTransition)
+    });
+
+window.addEventListener('keydown',playNow);
+
